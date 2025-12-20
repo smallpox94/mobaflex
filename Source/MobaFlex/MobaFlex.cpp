3c7b198a7e049b0c368222e641be4b0df66ddd2f
Smallpox94 <courtois.sebastien@gmail.com>
2025-12-19 21:18:43 -0500

    Initial Commit - Part 10

---
+++ Source/MobaFlex/MobaFlex.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MobaFlex.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MobaFlex, "MobaFlex" );
